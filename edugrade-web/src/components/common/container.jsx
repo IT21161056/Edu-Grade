5d981f40d336fdff41bab31681903f168918e9ac
AnojPeiris <it21161056@my.sliit.lk>
2024-04-16 13:17:15 +0530

    adjust some styles

--- edugrade-web/src/components/common/container.jsx
+++ edugrade-web/src/components/common/container.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Container = ({ children }) => {
-  return <div className=" max-w-[1440px] w-full">{children}</div>;
+  return <div className="max-w-screen-2xl w-full">{children}</div>;
 };
 
 export default Container;
