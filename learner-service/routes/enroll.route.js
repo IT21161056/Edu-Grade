08d269014bde43036c81b99d59acfe003561de58
Vikum Manchanayake <vikumviraj@vizuamatix.com>
2024-04-22 13:10:31 +0530

    course enrollment added

--- learner-service/routes/enroll.route.js
+++ learner-service/routes/enroll.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { enrollToCourse } from "../controllers/enroll.controller.js";
+import { enrollToCourse, unEnrollFromCourse } from "../controllers/enroll.controller.js";
 const router = express.Router();
 
 router.post("/enroll/:id",enrollToCourse)
+router.patch("/unEnroll",unEnrollFromCourse)
 
 export default router
