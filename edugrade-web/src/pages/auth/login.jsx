8a0286cd67a2cea399a0edb804e403404b15788b
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2024-04-26 11:34:57 +0530

    login, register

--- edugrade-web/src/pages/auth/login.jsx
+++ edugrade-web/src/pages/auth/login.jsx
@@ -1,41 +1,24 @@
-import React from "react";
-import {
-  Card,
-  Input,
-  Checkbox,
-  Button,
-  Typography,
-} from "@material-tailwind/react";
+import { Card, Input, Button, Typography } from "@material-tailwind/react";
 
 const Login = () => {
   return (
-    <div>
+    <div className="flex items-center justify-center mt-10">
       <Card color="transparent" shadow={false}>
-        <Typography variant="h4" color="blue-gray">
-          Sign Up
+        <Typography variant="h4" olor="blue-gray">
+          Sign In
         </Typography>
         <Typography color="gray" className="mt-1 font-normal">
-          Nice to meet you! Enter your details to register.
+          Enter your details to login.
         </Typography>
         <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
           <div className="mb-1 flex flex-col gap-6">
             <Typography variant="h6" color="blue-gray" className="-mb-3">
-              Your Name
+              Your Name or email-address
             </Typography>
             <Input
+              type="text"
               size="lg"
-              placeholder="name@mail.com"
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-              labelProps={{
-                className: "before:content-none after:content-none",
-              }}
-            />
-            <Typography variant="h6" color="blue-gray" className="-mb-3">
-              Your Email
-            </Typography>
-            <Input
-              size="lg"
-              placeholder="name@mail.com"
+              placeholder="name or email"
               className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
               labelProps={{
                 className: "before:content-none after:content-none",
@@ -47,40 +30,22 @@ const Login = () => {
             <Input
               type="password"
               size="lg"
-              placeholder="********"
+              placeholder="*******"
               className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
             />
+            <Button className="mt-6" fullWidth>
+              log in
+            </Button>
+            <Typography color="gray" className="mt-4 text-center font-normal">
+              Do not have an account?
+              <a href="/register" className="font-medium text-gray-900">
+                Sign Up
+              </a>
+            </Typography>
           </div>
-          <Checkbox
-            label={
-              <Typography
-                variant="small"
-                color="gray"
-                className="flex items-center font-normal"
-              >
-                I agree the
-                <a
-                  href="#"
-                  className="font-medium transition-colors hover:text-gray-900"
-                >
-                  &nbsp;Terms and Conditions
-                </a>
-              </Typography>
-            }
-            containerProps={{ className: "-ml-2.5" }}
-          />
-          <Button className="mt-6" fullWidth>
-            sign up
-          </Button>
-          <Typography color="gray" className="mt-4 text-center font-normal">
-            Already have an account?{" "}
-            <a href="#" className="font-medium text-gray-900">
-              Sign In
-            </a>
-          </Typography>
         </form>
       </Card>
     </div>
