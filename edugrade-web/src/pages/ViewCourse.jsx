11668ae591f697fd5dcf115984cd61322faec3ee
Vikum Manchanayake <vikumviraj@vizuamatix.com>
2024-05-07 08:58:55 +0530

    enrollment added

--- edugrade-web/src/pages/ViewCourse.jsx
+++ edugrade-web/src/pages/ViewCourse.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import {
   Card,
@@ -8,41 +8,68 @@ import {
   Button,
 } from "@material-tailwind/react";
 import axios from 'axios';
+import Content from '../components/Content';
+import { AuthContext } from '../context/authContext';
 
 const ViewCourse = () => {
 
   const { id } = useParams()
   const [loading, setLoading] = useState(false)
-  const [course,setCourse] = useState([])
- 
+  const [course, setCourse] = useState([])
+
   const navigate = useNavigate()
-  
-  const getCourse = async() => {
-    try{
+
+  const { user } = useContext(AuthContext);
+
+  const [enroll, setEnroll] = useState({
+    userId: user._id,
+    userEmail: user.email,
+    userMobile: user.mobile
+  })
+
+  const getCourse = async () => {
+    try {
       await axios.get(`http://localhost:8000/api/course/v1/${id}`)
-      .then((res) => {
-        setCourse(res.data)
-      })
-    }catch(err){
-      console.log('error fetching the course',err)
+        .then((res) => {
+          setCourse(res.data)
+        })
+    } catch (err) {
+      console.log('error fetching the course', err)
     }
   }
-
-  useEffect(() => {
-    getCourse()
-  },[])
 
   const back = () => {
     navigate('/view-course')
   }
 
-  
+  useEffect(() => {
+    getCourse()
+  }, [])
+
+  const enrollToCourse = async () => {
+    try {
+      setLoading(true)
+      await axios.post(`http://localhost:8000/api/learner-service/enrollment/enroll/${id}`,enroll)
+        .then((res) => {
+          if (res.status === 200) {
+            alert('enroll success')
+          }
+          setLoading(false)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <div>
       <Button onClick={back} className='relative top-4 ml-7'>Back</Button>
 
       <div className='flex justify-center items-center'>
-        <Card className="w-full max-w-[50rem] flex-row mt-20">
+        <Card className="w-full max-w-[50rem] flex-row mt-16">
           <CardHeader
             shadow={false}
             floated={false}
@@ -56,18 +83,30 @@ const ViewCourse = () => {
           </CardHeader>
           <CardBody>
             <Typography variant="h4" color="blue-gray" className="mb-2">
-            {course.courseName}
+              {course.courseName}
             </Typography>
             <Typography color="gray" className="mb-8 font-normal">
-             {course.courseDescription}
+              {course.courseDescription}
             </Typography>
             <a href="#" className="inline-block">
-              <Button loading={loading}>Enroll</Button>
+              <Button loading={loading} onClick={enrollToCourse}>Enroll</Button>
             </a>
           </CardBody>
         </Card>
       </div>
-
+      <Typography variant="h4" className='mt-5 ml-64 text-gray-600' color="blue-gray">Course Content</Typography>
+      <div className='grid place-items-center'>
+        {
+          course.contents && course.contents.map((element, index) => (
+            <Content
+              index={element._id}
+              topic={element.topic}
+              contentDescription={element.contentDescription}
+              sourceVideo={element.source}
+            />
+          ))
+        }
+      </div>
     </div>
   );
 }
