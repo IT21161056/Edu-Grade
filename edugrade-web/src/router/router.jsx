b0161452e823bd72e82076bfcd02f9578a3fc83b
Vikum Manchanayake <vikumviraj@vizuamatix.com>
2024-05-08 23:27:24 +0530

    User table added

--- edugrade-web/src/router/router.jsx
+++ edugrade-web/src/router/router.jsx
@@ -15,6 +15,7 @@ import ViewCourse from "../pages/ViewCourse";
 import MyCourses from "../pages/my-courses";
 import WatchCourse from "../pages/watchCourse";
 import Success from "../pages/paymentSuccess";
+import { ManageUser } from "../pages/ManageUser";
 
 const Router = () => {
   const ProtectedRoute = ({ children }) => {
@@ -40,6 +41,8 @@ const Router = () => {
 
       <Route path="/success" element={<Success />} />
 
+      <Route path="/manage-user" element={<ManageUser/>} />
+      
       <Route
         path="/create-course"
         element={
