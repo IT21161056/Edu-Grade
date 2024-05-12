9f33a77dbc32bc693599a4c264efc0e22f142d55
AnojPeiris <it21161056@my.sliit.lk>
2024-05-12 13:15:36 +0530

    chenges

--- edugrade-web/src/components/ui/adminNavbar.jsx
+++ edugrade-web/src/components/ui/adminNavbar.jsx
@@ -9,13 +9,6 @@ import React, { useContext, useState } from "react";
 import { AuthContext } from "../../context/authContext";
 import { Link, useNavigate, useNavigation } from "react-router-dom";
 
-const list = [
-  {
-    label: "My Learnings",
-    href: "/my-courses",
-  },
-];
-
 export default function AdminNavbar() {
   const [openNav, setOpenNav] = useState(false);
   const navigate = useNavigate();
@@ -40,24 +33,6 @@ export default function AdminNavbar() {
     );
   }, []);
 
-  const navList = (
-    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-      {list.map((navItem) => (
-        <Typography
-          as="li"
-          variant="small"
-          color="blue-gray"
-          className="p-1 font-normal"
-          key={navItem.label}
-        >
-          <Link to={navItem.href} className="flex items-center">
-            {navItem.label}
-          </Link>
-        </Typography>
-      ))}
-    </ul>
-  );
-
   return (
     <Navbar className="sticky top-0 z-20 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
       <div className="flex items-center justify-between text-blue-gray-900">
@@ -69,7 +44,6 @@ export default function AdminNavbar() {
           EDU GRADE
         </Typography>
         <div className="flex items-center gap-4">
-          <div className="mr-4 hidden lg:block">{navList}</div>
           <div className="flex items-center gap-x-1">
             {user ? (
               <Button
@@ -143,7 +117,6 @@ export default function AdminNavbar() {
         </div>
       </div>
       <Collapse open={openNav}>
-        {navList}
         <div className="flex items-center gap-x-1">
           {user ? (
             <Button
