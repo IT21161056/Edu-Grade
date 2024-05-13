81e9897794b10994482df5e32be0074244446335
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2024-05-13 08:53:41 +0530

    user dash

--- edugrade-web/src/pages/ManageUser.jsx
+++ edugrade-web/src/pages/ManageUser.jsx
@@ -5,126 +5,127 @@ import { useEffect, useState } from "react";
 
 const TABLE_HEAD = ["Name", "Email", "Mobile", "Role", "Action"];
 export const ManageUser = () => {
+  const [users, setUsers] = useState([]);
+  console.log(users);
 
-    const [users,setUsers] = useState([])
-    console.log(users)
-    
-    const getUsers = async() => {
-        await axios.get('http://localhost:8000/api/user/allProfiles')
+  const getUsers = async () => {
+    await axios
+      .get("http://localhost:8000/api/user/allProfiles")
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const deleteUser = async (id) => {
+    try {
+      await axios
+        .delete(`http://localhost:8000/api/user/${id}`)
         .then((res) => {
-            setUsers(res.data)
+          if (res.status === 200) {
+            const filterUser = users.filter((Obj) => Obj._id !== id);
+            setUsers(filterUser);
+          }
         })
         .catch((err) => {
-            console.error(err)
-        })
-    }
+          console.log("error deleting user");
+        });
+    } catch (err) {}
+  };
 
-    useEffect(() => {
-        getUsers()
-    },[])
+  return (
+    <div className="h-full w-full overflow-scroll p-4">
+      <Typography className="font-bold text-xl mb-4">Manage Users</Typography>
+      <Card className="h-full w-full overflow-scroll">
+        <table className="w-full min-w-max table-auto text-left mt-4">
+          <thead>
+            <tr>
+              {TABLE_HEAD.map((head) => (
+                <th
+                  key={head}
+                  className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
+                >
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal leading-none opacity-70"
+                  >
+                    {head}
+                  </Typography>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(({ name, email, mobile, role, _id }, index) => {
+              const isLast = index === users.length - 1;
+              const classes = isLast
+                ? "p-4"
+                : "p-4 border-b border-blue-gray-50";
 
-    const deleteUser = async(id) => {
-        try{
-            await axios.delete(`http://localhost:8000/api/user/${id}`)
-            .then((res) => {
-                if(res.status === 200){
-                    const filterUser = users.filter((Obj) => Obj._id !== id)
-                    setUsers(filterUser)
-                }
-            })
-            .catch((err) => {
-                console.log('error deleting user')
-            })
-        }catch(err){
-
-        }
-    }
-
-    return (
-        <div className="h-full w-full overflow-scroll p-4">
-                <Typography className="font-bold text-xl mb-4">User management</Typography>
-            <Card className="h-full w-full overflow-scroll">
-                <table className="w-full min-w-max table-auto text-left mt-4">
-                    <thead>
-                        <tr>
-                            {TABLE_HEAD.map((head) => (
-                                <th
-                                    key={head}
-                                    className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
-                                >
-                                    <Typography
-                                        variant="small"
-                                        color="blue-gray"
-                                        className="font-normal leading-none opacity-70"
-                                    >
-                                        {head}
-                                    </Typography>
-                                </th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users.map(({ name, email, mobile, role,_id }, index) => {
-                            const isLast = index === users.length - 1;
-                            const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
-
-                            return (
-                                <tr key={name}>
-                                    <td className={classes}>
-                                        <Typography
-                                            variant="small"
-                                            color="blue-gray"
-                                            className="font-normal"
-                                        >
-                                            {name}
-                                        </Typography>
-                                    </td>
-                                    <td className={classes}>
-                                        <Typography
-                                            variant="small"
-                                            color="blue-gray"
-                                            className="font-normal"
-                                        >
-                                            {email}
-                                        </Typography>
-                                    </td>
-                                    <td className={classes}>
-                                        <Typography
-                                            variant="small"
-                                            color="blue-gray"
-                                            className="font-normal"
-                                        >
-                                            {mobile}
-                                        </Typography>
-                                    </td>
-                                    <td className={classes}>
-                                        <Typography
-                                            as="a"
-                                            href="#"
-                                            variant="small"
-                                            color="blue-gray"
-                                            className="font-medium"
-                                        >
-                                            {role}
-                                        </Typography>
-                                    </td>
-                                    <td className={classes}>
-                                        <Typography
-                                            as="a"
-                                            href="#"
-                                            variant="small"
-                                            color="blue-gray"
-                                            className="font-medium cursor-pointe text-neutral-950"
-                                        >
-                                            <Trash2 color="red" onClick={() => deleteUser(_id)}/>
-                                        </Typography>
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </Card>
-        </div>
-    );
-}
+              return (
+                <tr key={name}>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {name}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {email}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {mobile}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      as="a"
+                      href="#"
+                      variant="small"
+                      color="blue-gray"
+                      className="font-medium"
+                    >
+                      {role}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      as="a"
+                      href="#"
+                      variant="small"
+                      color="blue-gray"
+                      className="font-medium cursor-pointe text-neutral-950"
+                    >
+                      <Trash2 color="red" onClick={() => deleteUser(_id)} />
+                    </Typography>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </Card>
+    </div>
+  );
+};
